c74e2c4182199dd9bd03f19dcfe928f3f11868b6
Fauna <fauna@rpjios.com>
2020-06-30 00:01:21 +0000

    Migration to separate repos, client step

--- server/tools/examine.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  runTool: async (opts) => {
-    console.log((await opts.server.getRuntimeInfo()));
-  },
-  displayName: () => 'Examine'
-};
--- server/tools/persist.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  runTool: async (opts) => {
-    console.log('persist#runTool');
-  },
-  displayName: () => 'Save & Restore'
-};
